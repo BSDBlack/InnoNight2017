ae03fbb41a9787818b8d07c60de0d69160926431
Kreuzer, Sebastian <sebastian.kreuzer@student.fhws.de>
2017-06-24 01:53:20 +0200

    added functionality for interaction

---
+++ FHWS_Vorlesungsplan/src/main/java/de/fhws/applab/skills/dataStructure/VPSessionBean.java
@@ -0,0 +1,17 @@
+package de.fhws.applab.skills.dataStructure;
+
+/**
+ * Created by Sebastian Kreuzer on 6/24/17.
+ */
+public class VPSessionBean {
+    private String UserId;
+
+
+    public String getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(String userId) {
+        UserId = userId;
+    }
+}
